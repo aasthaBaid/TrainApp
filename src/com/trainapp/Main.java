69148af48314950a263c67c1e64faa918a2906dc
aasthaBaid <aasthabaid3@gmail.com>
2026-03-10 17:09:53 +0530

    using groupingBy in stream

--- src/com/trainapp/Main.java
+++ src/com/trainapp/Main.java
@@ -1,13 +1,14 @@
 package com.trainapp;
 
 import java.util.*;
+import java.util.Map.Entry;
 //import java.util.Scanner;
 import java.util.stream.Collectors;
 
 
-/* UC8: Filter Passenger Bogies Using Streams
+/* UC9: Group Bogies by Type (Collectors.groupingBy)
  * @author developer
- * @version 8.0
+ * @version 9.0
  * 
  * */
 public class Main {
@@ -20,24 +21,31 @@ public class Main {
 		Bogie b2 = new Bogie("AC Chair" , 56);
 		Bogie b3 = new Bogie("First Class" , 24);
 		Bogie b4 = new Bogie("General" , 90);
+		Bogie b5 = new Bogie("First Class" , 60);
+		Bogie b6 = new Bogie("Sleeper" , 70);
 
 		bogies.add(b1);
 		bogies.add(b2);
 		bogies.add(b3);
 		bogies.add(b4);
+		bogies.add(b5);
+		bogies.add(b6);
 		System.out.println("Bogie with capacity without filtering:");
 		for(Bogie b : bogies) {
 			System.out.println(b.name +" -> " + b.capacity);
 		}
-		// filtered using stream, where capacity > 60 and again converted into list
-		List<Bogie> filtered = bogies.stream()
-								.filter(b -> b.capacity > 60)
-								.collect(Collectors.toList());
+		// grouping the list, using stream and groupedBy function in collectors and storing it in a map
+		Map<String,List<Bogie>> groupedBogies = bogies.stream()
+								.collect(Collectors.groupingBy(b -> b.name));
 		
-		System.out.println("\nBogie with capacity with filtering using stream (capacity > 60):");
-		// printing the filtered list
-		for(Bogie b : filtered) {
-			System.out.println(b.name +" -> " + b.capacity);
+		System.out.println("\nGrouping the list using bogie names:");
+		// printing the grouped map
+		for(Map.Entry<String, List<Bogie>> map : groupedBogies.entrySet()) {
+			System.out.println("\nBogie: "+ map.getKey());
+			for(Bogie b : map.getValue()) {
+				System.out.println("[" + b.name + " -> " + b.capacity + "]");
+			}
+			
 		}
 		
 	}	
