58a3e78a120e766c0be1c175b9e37271a37898ea
aasthaBaid <aasthabaid3@gmail.com>
2026-03-10 14:18:14 +0530

    using hashset to insert bogie id's with no duplicates

--- src/com/trainapp/Main.java
+++ src/com/trainapp/Main.java
@@ -4,9 +4,9 @@ import java.util.*;
 import java.util.Scanner;
 
 
-/* UC2: Add Passenger Bogies to Train (ArrayList Operations)
+/* UC3: Track Unique Bogie IDs (Set – HashSet)
  * @author developer
- * @version 2.0
+ * @version 3.0
  * 
  * */
 public class Main {
@@ -15,33 +15,18 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("--- Train Management App! ---\n");
-		// using list for dyanmic bogies entry
-		List<String> trainConsist = new ArrayList<>();
-		// initialising train app.
-		System.out.println("Train initiated successfully!");
-
-		// adding bogies to list 
-		trainConsist.add("Sleeper");
-		trainConsist.add("AC Chair");
-		trainConsist.add("First class");
-		System.out.println("Current Train Consist:" + trainConsist);
-
-		// removing AC chair
-		System.out.println("After removing AC chair:");
-		trainConsist.remove("AC Chair");
-		System.out.println("Current Train Consist:" + trainConsist);
-		// checking if sleeper exists:
-		
-		System.out.println("Does sleeper exists:");
-		boolean hasSleeper = trainConsist.contains("Sleeper");
-		if(hasSleeper) {
-			System.out.println("Sleeper present?: True");
-		}
-		else {
-			System.out.println("Sleeper present?: False");
-		}
-		// printing final train bogies.
-		System.out.println("Final train bogies:" + trainConsist);
+		// creating a hashset to avoid duplicate entries of the bogies
+		HashSet<String> bogies = new HashSet<String>();
+		bogies.add("BG101");
+		bogies.add("BG102");
+		bogies.add("BG103");
+		bogies.add("BG104");
+		bogies.add("BG102");
+		bogies.add("BG105");
+		// printing the bogies
+		// prints all the bogies in the hashset, removing duplicates.
+		System.out.println("Bogie ID's: " + bogies);
+		System.out.println("Using HashSet, ignored the duplicates added.");
 	}
 
 }
