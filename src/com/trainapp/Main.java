001e14300c186cd4b3adedebcab82108e2b8f84f
aasthaBaid <aasthabaid3@gmail.com>
2026-03-12 15:02:13 +0530

    applying illegalstateexception, to avoid fail fast behaviour

--- src/com/trainapp/Main.java
+++ src/com/trainapp/Main.java
@@ -2,9 +2,9 @@ package com.trainapp;
 
 import java.util.*;
 
-/* UC19: Binary Search for Bogie ID (Optimized Searching)
+/* UC20: Exception Handling During Search Operations
  * @author developer
- * @version 19.0
+ * @version 20.0
  * */
 public class Main {
 
@@ -12,34 +12,50 @@ public class Main {
 
 		System.out.println(" --- Train Management App! ---\n");
 		// making a list of bogie names.
-		String[] bogieIds = {"BG101", "BG609" , "BG307" , "BG455" , "BG488"};
+		//String[] bogieIds = {"BG101", "BG609" , "BG307" , "BG455" , "BG488"};
+		String[] bogieIds = {};
 		Arrays.sort(bogieIds);
 		String searchId = "BG455";
-		System.out.println("Bogie ids (Sorted):");
-		for(String id : bogieIds) {
-			System.out.print(id + " ");
-		}
-		System.out.println();
-		boolean found = false;
-		// doing a binary search to find the bogie id
-		int start = 0;
-		int end = bogieIds.length;
-		while(start <= end) {
-			int mid = start + ((end - start) / 2);
-			if(bogieIds[mid].equals(searchId)) {
-				found = true;
-				break;
+
+		try {
+			// if bogieIds length is 0, throws a IllegalStateException 
+			if (bogieIds.length == 0) {
+				throw new IllegalStateException("Cannot search — No bogies are present in the train consist.");
 			}
-			else if(bogieIds[mid].compareTo(searchId) < 0) {
-				start = mid + 1;
+
+			System.out.println("Bogie ids (Sorted):");
+			for(String id : bogieIds) {
+				System.out.print(id + " ");
 			}
-			else end = mid - 1;
+			System.out.println();
+			boolean found = false;
+			// doing a binary search to find the bogie id
+			int start = 0;
+			int end = bogieIds.length;
+			while(start <= end) {
+				int mid = start + ((end - start) / 2);
+				if(bogieIds[mid].equals(searchId)) {
+					found = true;
+					break;
+				}
+				else if(bogieIds[mid].compareTo(searchId) < 0) {
+					start = mid + 1;
+				}
+				else end = mid - 1;
+			}
+
+			// if the bogie id is found, then prints found
+			if(found) {
+				System.out.println(searchId + " Found in train consist");
+			}
+			else System.out.println(searchId + " Not found in train consit");
+
+		} catch (IllegalStateException ex) {
+			// Display meaningful message
+			System.out.println("Error: " + ex.getMessage());
 		}
-		
-		// if the bogie id is found, then prints found
-		if(found) {
-			System.out.println(searchId + " Found in train consist");
-		}
-		else System.out.println(searchId + " Not found in train consit");
 	}
 }
+
+
+
