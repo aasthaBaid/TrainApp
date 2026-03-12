74bf62a6b3fe4932d6d30425f8d290cfd96c7d45
aasthaBaid <aasthabaid3@gmail.com>
2026-03-12 12:28:21 +0530

    sorting capacites using bubble sort

--- src/com/trainapp/CargoSafetyException.java
+++
@@ -1,9 +0,0 @@
-package com.trainapp;
-
-public class CargoSafetyException extends Exception{
-	public CargoSafetyException(String message) {
-		super(message);
-	}
-}
-
-
--- src/com/trainapp/Main.java
+++ src/com/trainapp/Main.java
@@ -2,9 +2,9 @@ package com.trainapp;
 
 import java.util.*;
 
-/* UC15: Safe Cargo Assignment Using try-catch-finally
+/* UC16: Sort Passenger Bogies by Capacity (Bubble Sort – Algorithm Intro)
  * @author developer
- * @version 15.0
+ * @version 16.0
  * 
  * */
 public class Main {
@@ -12,17 +12,34 @@ public class Main {
 	public static void main(String[] args){
 
 		System.out.println(" --- Train Management App! ---\n");
-		// trying to create a boggie for sleeper
-		GoodsBogie b1 = new GoodsBogie("Cylindrical");
-		b1.assignCargo("Petroleum");
+		
+		// Create array of passenger bogie capacities
+        int[] capacities = {72, 56, 24, 70, 60};
 
-		System.out.println();
+        // Display original order
+        System.out.println("Original Capacities:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
 
-		// INVALID assignment (will trigger exception)
-		GoodsBogie b2 = new GoodsBogie("Rectangular");
-		b2.assignCargo("Petroleum");   // unsafe
+        // Outer loop controls number of passes
+        for (int i = 0; i < capacities.length - 1; i++) {
+            // Inner loop compares adjacent elements
+            for (int j = 0; j < capacities.length - 1 - i; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    // swap
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        // Display sorted result
+        System.out.println("\n\nSorted Capacities (Ascending):");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
 
 	}
 }
-
-
